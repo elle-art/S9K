69a1d9952592689a298d6c01846760c531f6952f
Carl Ghess <carl.ghess@gmail.com>
2025-04-29 16:50:23 -0500

    Implemented functions in AvailabilityService, CalendarService, and EventService classes

--- backend/GroupProjectAPI/Services/DBCommunications.cs
+++ backend/GroupProjectAPI/Services/DBCommunications.cs
@@ -16,16 +16,31 @@ namespace Backend.Services
         }
 
         /// <summary>
-        /// saves an object to firestore
+        /// saves an object to firebase
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="uid">the user id</param>
-        /// <param name="className">the class name of the object. Case sensitive</param>
+        /// <param name="className">the class name of the object. Case insensitive</param>
         /// <param name="obj">the object to be saved</param>
         /// <returns></returns>
         public static async Task SaveObjectAsync<T>(string uid, string className, T obj)
         {
+            className = className.ToLower();
             await FirebaseCommunications.SaveToFirestore(obj, uid, className);
+        }
+
+        /// <summary>
+        /// Saves an object to firebase
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="uid"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static async Task SaveObjectAsync<T>(string uid, T obj) 
+        {
+            string className = typeof(T).Name.ToLower();
+            await FirebaseCommunications.SaveToFirestore(obj, uid, className);
+
         }
 
         /// <summary>
@@ -33,10 +48,11 @@ namespace Backend.Services
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="uid">the user's id</param>
-        /// <param name="className">the name of the class</param>
+        /// <param name="className">the name of the class. Case insensitive</param>
         /// <returns></returns>
         public static async Task<T> GetObjectAsync<T>(string uid, string className)
         {
+            className = className.ToLower();
             return await FirebaseCommunications.LoadFromFirestore<T>(uid, className, "primary");
         }
 
--- backend/GroupProjectAPI/models/AvailabilityService.cs
+++ backend/GroupProjectAPI/models/AvailabilityService.cs
@@ -5,37 +5,49 @@ using Backend.Services;
 public class AvailabilityService
 {
 
-    //To-Do: Firebase shenanigans to find if an existing availability is stored for this user (That search can be implemented in the controller)
-    public async Task<Availability> CreateAvailability(List<TimeBlock>[] schedule, bool FBEntryExists = false)
+    /// <summary>
+    /// Gets an existing availability from firebase
+    /// </summary>
+    /// <param name="uid">user's ID</param>
+    /// <returns></returns>
+    public static async Task<Availability> GetAvailabilityAsync(string uid) 
     {
-        // FirebaseCommunications db = new FirebaseCommunications();
-
-        if (FBEntryExists)
-        {
-            //aforementioned FireBase shenanigans go here
-
-            //temp return to bypass error
-            return null;
-        }
-        else
-        {
-            //To-do: Store this in the user info as userAvailability.
-            // DocumentReference docRef = db.Collection("availabilities").Document('a'); // check collection name in FB + needs id/name
-            Availability availability = new Availability
-            {
-                weeklySchedule = schedule
-            };
-
-            // await docRef.SetAsync(availability);
-
-            return availability;
-        }
+        return await DBCommunications.GetObjectAsync<Availability>(uid, "Availability");
     }
 
-    public void UpdateAvailability(ref Availability userAvailability, int day, TimeBlock timeBlock, bool isDelete = false)
+    /// <summary>
+    /// Create a new availability object and save it to firebase
+    /// </summary>
+    /// <param name="uid"></param>
+    /// <param name="schedule"></param>
+    /// <returns></returns>
+    public static async Task<Availability> CreateAvailabilityAsync(string uid, List<TimeBlock>[] schedule) 
     {
-        //Tentative implementation
+        Availability availability = new Availability 
+        {
+            weeklySchedule = schedule
+        };
+
+        await DBCommunications.SaveObjectAsync(uid, availability);
+
+        return availability;
+    }
+
+
+    /// <summary>
+    /// Updates the user availability and saves it to firestore
+    /// </summary>
+    /// <param name="uid"></param>
+    /// <param name="userAvailability"></param>
+    /// <param name="day"></param>
+    /// <param name="timeBlock"></param>
+    /// <param name="isDelete"></param>
+    /// <returns></returns>
+    public static async Task UpdateAvailabilityAsync(string uid, Availability userAvailability, int day, TimeBlock timeBlock, bool isDelete = false) 
+    {
         userAvailability.EditAvailability(day, timeBlock, isDelete);
+
+        await DBCommunications.SaveObjectAsync(uid, userAvailability);
     }
 
 }
